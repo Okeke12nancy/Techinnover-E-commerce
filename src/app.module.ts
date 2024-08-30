edf6e9e0f5667fc36bc0fe5c07e51524454fcfd7
Nancy Okeke <ogadimmanancy12@gmail.com>
2024-08-30 21:24:02 +0100

    Initial project setup with NestJs, Typeorm, PostgreSql

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME || 'your-username',
+      password: process.env.DB_PASSWORD || 'your-password',
+      database: process.env.DB_NAME || 'ecommerce',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
